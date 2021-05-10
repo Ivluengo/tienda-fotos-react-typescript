9e8cfd3c010a50468f6f75e0c3856fd796849ecc
ivluengo <ivluengo@gmail.com>
2021-05-10 13:58:27 +0200

    Añadidos comentarios sobre HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import * as ReactDOM from 'react-dom';
 //podemos usar BrowserRouter directamente pero en muchos sitios se crea el alias de Router para simplificar
-import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import { /*BrowserRouter as Router,*/ HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 //Nuestro Router siempre tiene que envolver aquello que queramos que pueda usar Route, Switch, etc.
+//En este caso, solo porque necesito hacer el deploy de la app dentro de Github Pages uso "HashRouter" en lugar de "BrowserRouter" pero lo normal es siempre usar "BrowserRouter".
 ReactDOM.render(<HashRouter basename='/'><App /></HashRouter>, document.getElementById('root'));
