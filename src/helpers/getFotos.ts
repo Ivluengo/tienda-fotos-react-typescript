fe8ff4daf481a4a553b667e7956c1c9687a851e9
ivluengo <ivluengo@gmail.com>
2021-05-10 12:45:11 +0200

    Cabmiadas url de las fotos

--- src/helpers/getFotos.ts
+++ src/helpers/getFotos.ts
@@ -1,7 +1,7 @@
 //Importamos nuestras fotos en este caso de un repositorio de mi cuenta en la que tengo una serie de fotos
 
 const getFotos = async () => {
-    const resp = await fetch('https://raw.githubusercontent.com/Ivluengo/react-tienda-fotos/main/listaFotos.json');
+    const resp = await fetch('https://raw.githubusercontent.com/Ivluengo/tienda-fotos-react-typescript/main/src/fotos-animales/listaFotos.json');
     const fotos = await resp.json();
 
     return fotos;
