bf6ecfaacd68be9714ad358de159c882dc23a09e
ivluengo <ivluengo@gmail.com>
2021-05-10 13:54:04 +0200

    Eliminada carpeta docs y haciendo deploy driectamente con gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as ReactDOM from 'react-dom';
 //podemos usar BrowserRouter directamente pero en muchos sitios se crea el alias de Router para simplificar
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 //Nuestro Router siempre tiene que envolver aquello que queramos que pueda usar Route, Switch, etc.
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename='/'><App /></HashRouter>, document.getElementById('root'));
